8b1b191259e95b97a48e1e6c806476f4ee248558
MiMad89 <michal9977@gmail.com>
2023-06-16 01:13:58 +0200

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,25 @@
-import { Component } from "react";
-import {Contacts} from './Contacts'
-import {Filter} from './Filter'
+import { Component } from 'react';
+import { Contacts } from './Contacts';
+import { Filter } from './Filter';
 
+
+const INITIAL_STATE = {
+  contacts: [],
+  name: '',
+}; 
 export class App extends Component {
+
+  
+
+  state = {
+    ...INITIAL_STATE
+  };
+
   render() {
     return (
       <div>
         <h1>Phonebook</h1>
-        <Contacts />
+        <Contacts  />
         <Filter />
       </div>
     );
